ba59ed76a1a9442323ae0a7d9df007477b133e49
Ryan Kienstra <kienstraryan@gmail.com>
2020-12-24 23:46:19 -0600

    Apply suggestions from the video
    Based on Kent Dodds' solution

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,8 +4,11 @@
 import * as React from 'react'
 
 const countReducer = (state, action) => {
-  if (action.type === 'INCREMENT') {
-    return {count: state.count + action.step}
+  switch (action.type) {
+    case 'INCREMENT':
+      return {count: state.count + action.step}
+    default:
+      throw new Error(`Unsupported action type: ${action.type}`)
   }
 }
 
